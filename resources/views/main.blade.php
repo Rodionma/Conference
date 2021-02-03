082681d9690bf09e5a2f5ff27204c6f4df781ea2
Rodion <makarenko.rodion98@gmail.com>
2021-02-03 11:37:14 +0200

    added second form

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -1,36 +1,8 @@
-<!DOCTYPE HTML>
-<html lang="ru">
-<head>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
-
-    <link rel="apple-touch-icon" href="/docs/5.0/assets/img/favicons/apple-touch-icon.png" sizes="180x180">
-    <link rel="icon" href="/docs/5.0/assets/img/favicons/favicon-32x32.png" sizes="32x32" type="image/png">
-    <link rel="icon" href="/docs/5.0/assets/img/favicons/favicon-16x16.png" sizes="16x16" type="image/png">
-    <link rel="manifest" href="/docs/5.0/assets/img/favicons/manifest.json">
-    <link rel="mask-icon" href="/docs/5.0/assets/img/favicons/safari-pinned-tab.svg" color="#7952b3">
-    <link rel="icon" href="/docs/5.0/assets/img/favicons/favicon.ico">
-    <meta name="theme-color" content="#7952b3">
 
 
-    <style>
-        .bd-placeholder-img {
-            font-size: 1.125rem;
-            text-anchor: middle;
-            -webkit-user-select: none;
-            -moz-user-select: none;
-            user-select: none;
-        }
-        @media (min-width: 768px) {
-            .bd-placeholder-img-lg {
-                font-size: 3.5rem;
-            }
-        }
-    </style>
+@extends('layout')
 
-    <meta charset="utf-8"/>
-    <title>Conference register</title>
-</head>
-<body>
+@section('content')
 
 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d412.9714930200327!2d-118.344246624629!3d34.10098228558076!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80c2bf20e4c82873%3A0x14015754d926dadb!2zNzA2MCBIb2xseXdvb2QgQmx2ZCwgTG9zIEFuZ2VsZXMsIENBIDkwMDI4LCDQodCo0JA!5e0!3m2!1sru!2sua!4v1612343005131!5m2!1sru!2sua" width="100%" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
 
@@ -68,5 +40,8 @@
     <button class="btn btn-success" type="submit">Next</button>
 </form>
 
-</body>
-</html>
+
+
+@endsection
+
+
--- resources/views/second.blade.php
+++ resources/views/second.blade.php
@@ -1,1 +1,27 @@
-<?php
+@extends('layout')
+
+@section('content')
+
+<form method="post" action="/member/update">
+<div class="form-group">
+        <label for="company" class="form-label">Company</label>
+        <input type="text" name="company" class="form-control" id="company" placeholder="Please enter your company">
+    </div>
+
+    <div class="form-group">
+        <label for="lastname" class="form-label">Position</label>
+        <input type="text" name="lastname" class="form-control" id="position" placeholder="Please enter your position">
+    </div>
+
+    <div class="form-group">
+        <label for="birthdate" class="form-label">Birthdate*</label>
+        <input type="date" name="date" class="form-control" id="birthdate" placeholder="Please enter your birthdate">
+    </div>
+
+    <div class="form-group">
+        <label for="description" class="form-label">About me</label>
+        <textarea name="description" id="description" class="form-control" placeholder="About me" rows=6> </textarea>
+    </div>
+
+</form>
+    @endsection
